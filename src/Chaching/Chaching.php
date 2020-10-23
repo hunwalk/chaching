fac42b213350b4ef4f72e99f5bbecca2b5abf808
hunwalk <horg3r@gmail.com>
2020-10-23 15:13:00 +0200

    refactor: making it prettier

--- src/Chaching/Chaching.php
+++ src/Chaching/Chaching.php
@@ -11,86 +11,94 @@
 
 namespace Chaching;
 
-use \Chaching\Exceptions\InvalidOptionsException;
+use BadMethodCallException;
+use Chaching\Exceptions\InvalidOptionsException;
 
 
+/**
+ * Class Chaching
+ * @package Chaching
+ */
 class Chaching
 {
-	const VERSION 		= '0.20.1';
+    const VERSION = '0.20.1';
 
-	const CARDPAY 		= 'cardpay';
-	const SPOROPAY 		= 'sporopay';
-	const TATRAPAY 		= 'tatrapay';
-	const TRUSTPAY 		= 'trustpay';
-	const EPLATBY 		= 'eplatby';
-	const ECARD 		= 'ecard';
-	const PAYPAL		= 'paypal';
-	const GPWEBPAY 		= 'gpwebpay';
-	const ITERMINAL 	= 'iterminal';
+    const CARDPAY = 'cardpay';
+    const SPOROPAY = 'sporopay';
+    const TATRAPAY = 'tatrapay';
+    const TRUSTPAY = 'trustpay';
+    const EPLATBY = 'eplatby';
+    const ECARD = 'ecard';
+    const PAYPAL = 'paypal';
+    const GPWEBPAY = 'gpwebpay';
+    const ITERMINAL = 'iterminal';
 
-	const PRODUCTION 	= 'production';
-	const SANDBOX 		= 'sandbox';
+    const PRODUCTION = 'production';
+    const SANDBOX = 'sandbox';
 
-	private $payment_drivers = [
-		self::SPOROPAY 		=> 'SLSPSporoPay',
-		self::CARDPAY 		=> 'TBCardPay',
-		self::TATRAPAY 		=> 'TBTatraPay',
-		self::TRUSTPAY 		=> 'TrustPay',
-		self::EPLATBY 		=> 'VUBePlatby',
-		self::ECARD 		=> 'VUBeCard',
-		self::PAYPAL		=> 'PayPal',
-		self::GPWEBPAY 		=> 'GPwebpay',
-		self::ITERMINAL 	=> 'PBiTerminal'
-	];
+    /**
+     * @var string[]
+     */
+    private $payment_drivers = [
+        self::SPOROPAY => 'SLSPSporoPay',
+        self::CARDPAY => 'TBCardPay',
+        self::TATRAPAY => 'TBTatraPay',
+        self::TRUSTPAY => 'TrustPay',
+        self::EPLATBY => 'VUBePlatby',
+        self::ECARD => 'VUBeCard',
+        self::PAYPAL => 'PayPal',
+        self::GPWEBPAY => 'GPwebpay',
+        self::ITERMINAL => 'PBiTerminal'
+    ];
 
-	/**
-	 * Create object to work with payments via specified driver.
-	 *
-	 * @param 	int 		$driver 		driver handle
-	 * @param 	array 		$authorization 	basic authentication information
-	 * 										to service
-	 * @param 	array|NULL 	$additional_information 	additional information
-	 * 													to service
-	 **/
-	public function __construct($driver, Array $authorization, Array $options = [])
-	{
-		if (!is_string($driver) OR !isset($this->payment_drivers[ $driver ]))
-			throw new InvalidOptionsException(sprintf(
-				"Invalid driver '%s' in use. Valid drivers are '%s'.",
-				$driver, implode("', '", array_keys($this->payment_drivers))
-			));
+    /**
+     * Create object to work with payments via specified driver.
+     *
+     * @param int $driver driver handle
+     * @param array $authorization basic authentication information
+     *                                        to service
+     * @param array $options
+     */
+    public function __construct($driver, array $authorization, array $options = [])
+    {
 
-		$driver = '\\Chaching\\Drivers\\' . $this->payment_drivers[ $driver ];
+        if (!is_string($driver) or !isset($this->payment_drivers[$driver]))
+            throw new InvalidOptionsException(sprintf(
+                "Invalid driver '%s' in use. Valid drivers are '%s'.",
+                $driver, implode("', '", array_keys($this->payment_drivers))
+            ));
 
-		if (!class_exists($driver))
-			throw new InvalidOptionsException(sprintf(
-				"[internal] Requested driver '%s' does not appear to have " .
-				"class definition associated.",
-				$driver, implode("', '", array_keys($this->payment_drivers))
-			));
+        $driver = '\\Chaching\\Drivers\\' . $this->payment_drivers[$driver];
 
-		$this->driver = new $driver($authorization, $options);
-	}
+        if (!class_exists($driver))
+            throw new InvalidOptionsException(sprintf(
+                "[internal] Requested driver '%s' does not appear to have " .
+                "class definition associated. Valid drivers are %s",
+                $driver, implode("', '", array_keys($this->payment_drivers))
+            ));
 
-	public function __call($method, $arguments)
-	{
-		if (method_exists($this->driver, $method))
-			return call_user_func_array(
-				[ $this->driver, $method ], $arguments
-			);
+        $this->driver = new $driver($authorization, $options);
+    }
 
-		throw new \BadMethodCallException(sprintf(
-			"Method %s not implemented in driver", $method
-		));
-	}
+    public function __call($method, $arguments)
+    {
+        if (method_exists($this->driver, $method))
+            return call_user_func_array(
+                [$this->driver, $method], $arguments
+            );
 
-	public function request($attributes)
-	{
-		return $this->driver->request((array) $attributes);
-	}
+        throw new BadMethodCallException(sprintf(
+            "Method %s not implemented in driver", $method
+        ));
+    }
 
-	public function response($attributes)
-	{
-		return $this->driver->response((array) $attributes);
-	}
+    public function request($attributes)
+    {
+        return $this->driver->request((array)$attributes);
+    }
+
+    public function response($attributes)
+    {
+        return $this->driver->response((array)$attributes);
+    }
 }
